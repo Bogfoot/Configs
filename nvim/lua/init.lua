263287abc1aa8ca4dac249b50f3f4a1de6dacd53
bogfoot <bogfoot94@yahoo.com>
2023-04-04 20:39:03 +0200

    changes to nvim

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -3,12 +3,12 @@ local vim = vim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
@@ -17,13 +17,13 @@ vim.opt.termguicolors = true
 vim.opt.undofile = true
 require('plugins')
 require('settings')
-require('miscelanious')
 require('treesitter')
 require('keybinds')
+require('miscelanious')
 require('nvim-cmp')
 require('autocommands')
 require('autopairs')
---require('nvim-tree')
+require('nvim-tree')
 require('nvim-lualine')
 
 -- require('myplugins')
--- nvim/lua_packer/keybinds.lua
+++ nvim/lua_packer/keybinds.lua
@@ -1,4 +1,3 @@
-local vim = vim
 vim.cmd('noremap <C-b> :noh<cr>:call clearmatches()<cr>') -- clear matches Ctrl+b
 function map(mode, shortcut, command)
 	vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
@@ -29,16 +28,28 @@ nmap("<leader>n", ":nohl<cr>")
 -- Find files using Telescope command-line sugar.
 nmap("<leader>ff", "<cmd>Telescope find_files<cr>")
 nmap("<leader>fg", "<cmd>Telescope live_grep<cr>")
+nmap("<leader>fb", "<cmd>Telescope buffers<cr>")
 nmap("<leader>fh", "<cmd>Telescope help_tags<cr>")
+
+-- Nvim-Tree
+-- nmap("<C-t>", ":NvimTreeToggle<CR>")
+-- nmap("<C-n>", ":NvimTreeOpen<CR>")
+-- nmap("<C-f>", ":NvimTreeFocus<CR>")
 -- NERDTree naredbe
--- nmap("<C-t>", ":NERDTreeToggle <CR>")
-nmap("<C-t>", ":Neotree toggle<CR>")
+nmap("<C-f>", ":NERDTreeFocus <CR>")
+nmap("<C-n>", ":NERDTree <CR>")
+nmap("<C-t>", ":NERDTreeToggle <CR>")
+
 --Dap
 nmap("<leader>dbp", ":lua require'dap'.toggle_breakpoint()<CR>")
 nmap("<leader>dc", ":lua require'dap'.continue()<CR>")
 nmap("<leader>dso", ":lua require'dap'.step_over()<CR>")
+
+
 -- Moje naredbe
-vim.cmd([[ nmap <Leader>-c :%s/\s\+$// <CR> ]])
+vim.cmd([[
+nmap <Leader>-c :%s/\s\+$// <CR>
+]])
 nmap("<F3>", ":set nu! <CR>")
 nmap("<Leader>go", ":source vimbook.vim <CR>") --ova i iduća naredba stvaraju 'vimbook' file koji sadrži file i položaj u file u kojem se zadnje radilo (za koji se koristila iduća naredba)
 nmap("<Leader>end", ":w <CR>:mksession! vimbook.vim<CR>:q <CR>")
@@ -50,18 +61,3 @@ nmap("<Leader>cht", ":CheatSH<CR>")
 
 vmap(">", "<gv")
 vmap(">", ">gv")
-
-local term_map = require("terminal.mappings")
-vim.keymap.set({ "n", "x" }, "<leader>ts", term_map.operator_send, { expr = true })
-vim.keymap.set("n", "<leader>to", term_map.toggle)
-vim.keymap.set("n", "<leader>tO", term_map.toggle({ open_cmd = "enew" }))
-vim.keymap.set("n", "<leader>tr", term_map.run)
-vim.keymap.set("n", "<leader>tR", term_map.run(nil, { layout = { open_cmd = "enew" } }))
-vim.keymap.set("n", "<leader>tk", term_map.kill)
-vim.keymap.set("n", "<leader>t]", term_map.cycle_next)
-vim.keymap.set("n", "<leader>t[", term_map.cycle_prev)
-vim.keymap.set("n", "<leader>tl", term_map.move({ open_cmd = "belowright vnew" }))
-vim.keymap.set("n", "<leader>tL", term_map.move({ open_cmd = "botright vnew" }))
-vim.keymap.set("n", "<leader>th", term_map.move({ open_cmd = "belowright new" }))
-vim.keymap.set("n", "<leader>tH", term_map.move({ open_cmd = "botright new" }))
-vim.keymap.set("n", "<leader>tf", term_map.move({ open_cmd = "float" }))
