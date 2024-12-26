36d44148b7f44568abb7fb3afa6157efc7f050e0
bogfoot <bogfoot94@yahoo.com>
2024-12-26 15:35:12 +0100

    upd

--- nvim/lua/miscelanious.lua
+++ nvim/lua/miscelanious.lua
@@ -1,4 +1,4 @@
-vim.cmd([[
+vim.cmd [[
 set mouse=a
 hi clear Conceal
 
@@ -23,6 +23,8 @@ let g:webdevicons_enable_airline_statusline = 1
 let g:webdevicons_conceal_nerdtree_brackets = 1
 let g:WebDevIconsUnicodeDecorateFolderNodes = 1
 
+
+"let g:vimtex_mappings_prefix='\'.
 let g:vimtex_quickfix_open_on_warning = 0
 let g:airline#extensions#vimtex#enabled=1
 
@@ -40,25 +42,16 @@ let g:translator_target_lang='hr'
 let g:translator_default_engines='google'
 
 " press <Tab> to expand or jump in a snippet. These can also be mapped separately
-" via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-" -1 for jumping backwards.
-inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+"imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+"inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
-snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+"snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+"snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
 
 " For changing choices in choiceNodes (not strictly necessary for a basic setup).
-imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+"imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+"smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 
 let NERDTreeShowLineNumbers=1
 autocmd FileType nerdtree setlocal relativenumber
-]])
-
--- au FileType py,pyx,h,hpp,hxx,cxx,c,cpp,objc,objcpp call rainbow#load()
--- inoremap <expr> <CR> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
--- inoremap <expr> <C-n> pumvisible() ? '<C-n>' :
---   \ '<C-n><C-r>=pumvisible() ? "\<lt>Down>" : ""<CR>'
--- inoremap <expr> <M-,> pumvisible() ? '<C-n>' :
---   \ '<C-x><C-o><C-n><C-p><C-r>=pumvisible() ? "\<lt>Down>" : ""<CR>'
+]]
